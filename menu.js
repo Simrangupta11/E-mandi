06317b1a3600ae907b6d397dbd0e693f2e0840dd
Simrangupta11 <76567049+Simrangupta11@users.noreply.github.com>
2021-04-29 13:25:01 +0530

    this is a online grocery store

---
+++ menu.js
@@ -0,0 +1,12 @@
+function stickyMenu() {
+    var sticky=document.getElementById('sticky');
+    if(window.pageYOffset > 220) {
+        sticky.classList.add('sticky');
+    }
+    else {
+        sticky.classList.remove('sticky');
+    }
+}
+window.onscroll = function() {
+    stickyMenu () ;
+}
